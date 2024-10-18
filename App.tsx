53cb5eea6e73c5ffa74f0ad35c364d9128f4cd18
Kenny <kenny@Minas-MacBook-Pro.local>
2024-10-18 11:01:10 -0400

    remove react-query

--- App.tsx
+++ App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from './src/styles/ThemeProvider';
 
 import store from './src/app-state/store';
 import AppNavigator from './src/navigation';
 
-const queryClient = new QueryClient();
-
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <NavigationContainer>
-            <AppNavigator />
-          </NavigationContainer>
-        </ThemeProvider>
-      </QueryClientProvider>
+      <ThemeProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </ThemeProvider>
     </Provider>
   );
 };
