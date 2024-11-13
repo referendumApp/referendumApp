2eb5c01e30eafdc1b53316d37ce04f6b5c00c17b
Ken <k.ng993@gmail.com>
2024-11-13 18:10:16 -0500

    mock packages

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,27 @@
 import 'react-native-gesture-handler/jestSetup';
 
+jest.mock('@expo/vector-icons', () => ({
+  Ionicons: 'Ionicons',
+  Feather: 'Feather',
+  MaterialCommunityIcons: 'MaterialCommunityIcons',
+  // Add other icon families you use
+}));
+
 jest.mock('expo-font');
 jest.mock('expo-splash-screen');
 
 // Silence the warning: Animated: `useNativeDriver` is not supported
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+// Mock Navigation
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({ navigate: jest.fn() }),
+  NavigationContainer: ({ children }) => children,
+}));
+
+// Mock Redux
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => jest.fn(),
+  Provider: ({ children }) => children,
+}));
